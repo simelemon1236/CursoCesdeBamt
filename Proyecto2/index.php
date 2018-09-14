4f3c40990af30e5079bacdbfc3fa08124038d47a
Alejo MOra <beisonmoratazx@gmail.com>
2018-09-14 17:27:54 -0500

    cilos forwhile

--- Proyecto2/index.php
+++ Proyecto2/index.php
@@ -7,42 +7,78 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Proyecto</title>
+        <title></title>
     </head>
     <body>
-        
         <h1>
             <?php
-                //Estp es un comentario
-                $PrimeraVariable=2;
-                $SegundaVariable=5;
-                echo "<br>";
-                echo $PrimeraVariable + $SegundaVariable;
-                 echo "<br>";
-                 $Lenguaje="PHP";
-                if ($PrimeraVariable== 2 && $SegundaVariable==5){
-                    echo 'Soy menor'."<br>";
+                // Esto es un Comentario
+                $PrimeraVariable = 2;
+                 $SegundaVariable = 5;
+                 $Lenguaje = "HTML";
+                echo $PrimeraVariable / $SegundaVariable . "<br>" ;
+                if ($PrimeraVariable == 2 && $SegundaVariable == 5){
+                    echo 'soy menor'. "<br>";
                 }
-                else {
-                    echo 'Soy Mayor'."<br>";
+                else{
+                     echo 'soy mayor'. "<br>";
                 }
+                // -------switch------
                 switch ($Lenguaje){
                     case "PHP":
                         echo 'PHP';
                         break;
-                    case"Hmtl":
-                        echo 'Html';
+                    case"JAVASCRIPT" :
+                        echo 'JAVASCRIPT';
+                        break;
+                    case "HTML":
+                        echo 'HTML';
                         break;
                 }
-                    
-            ?>
+                // ------ ARREGLOS-----
+                echo '<br>';
+                $arreglo = [2 , "Hola", 5 ];
+                echo $arreglo [1] ;
+                print_r($arreglo)
+                //---------------------
+               // $arreglo = array ("hola", 1,"hola")
+                //echo $SegundaVariable + $SegundaVariable;
+            ?>   
         </h1>
         <p>
-            <h2>  
-                <?php
-                    echo 'Esto es otra sección';
+            <h1>
+                <?PHP
+                    echo 'Esta es otra seccion';
                 ?>
-            </h2> 
+             </h1>
         </p>
+        // ---------------imprimir areglos con posiciones las listas
+        <pre>
+            <?PHP
+                 
+                $dato=array_pop($arreglo);
+                print_r($arreglo);
+                //Arreglos asociativos
+                $usuarios= array ('nombre'=>"juan","Apellido"=>"zabala","Cedula"=>1234);
+                echo"<h1>".$usuarios ['nombre']."</h1>";
+                //-------------------------------------------
+                
+                $existe= in_array ("juan",array_values($usuarios));
+                var_dump($existe);
+                //-----Foreach
+                foreach ($usuarios as $user){
+                    echo $user."<br>";
+                }
+                
+            ?>
+            <ul>
+                <?php
+                    foreach($usuarios as  $user);
+                    echo "<li>".$user."</li>";
+                ?>
+                
+            </ul>
+        </pre>
     </body>
 </html>
+
